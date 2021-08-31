57aa83b59dc9a9f6dd7890ff00de82c083c3579d
Koray Aksoy <burakkorayaksoy@hotmail.com>
2021-08-31 15:44:00 +0300

    Add project files.

---
+++ Rabbitmq_Receive/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Rabbitmq_Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connFactory = new ConnectionFactory { HostName = "localhost", Port = 5672 };          
+            using (var connection = connFactory.CreateConnection())
+            {                
+                using (var channel = connection.CreateModel())
+                {                    
+                    TopicExchangeConsumer.Consume(channel);
+                }
+            }
+        }
+    }
+}
---
+++ Rabbitmq_Receive/TopicExchangeConsumer.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbitmq_Receive
+{
+    public static class TopicExchangeConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.ExchangeDeclare("social-topic-exchange", ExchangeType.Topic);
+            channel.QueueDeclare("social-topic-queue-instagram",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            channel.QueueBind("social-topic-queue-instagram", "social-topic-exchange", "instagram.*");
+            channel.BasicQos(0, 10, false);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+            channel.BasicConsume("social-topic-queue-instagram", true, consumer);
+            Console.WriteLine("consumer started");
+            Console.ReadLine();
+        }
+    }
+}
